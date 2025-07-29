843b0c931e50e9120c4ae266e0c2ff4731b096bf
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-07-29 10:42:03 +0100

    OLH-2970: Deploy the VPC template to all accounts
    Note I've set `AllowRules` and `AllowedDomains` to allow outbound
    traffic from these VPCs to our domains. This is because we'll need to be
    able to reach our clients' JWKS endpoints so we can verfy their signing
    keys.
    
    `*.account.gov.uk` is included in the default rules for the domain and
    network firewalls, but if both these parameters are empty then the
    template won't configure the VPC to allow outbound traffic.

---
+++ infra/vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudformation_stack" "vpc_stack" {
+  # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3531735041/VPC
+  provider     = aws.london
+  name         = "vpc"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/vpc/template.yaml"
+
+  parameters = {
+    CloudWatchApiEnabled = "Yes"
+    DynatraceApiEnabled  = "Yes"
+    KMSApiEnabled        = "Yes"
+    AllowRules           = "pass tls $HOME_NET any -> $EXTERNAL_NET 443 (tls.sni; content:\"account.gov.uk\"; endswith; msg:\"Pass TLS to *.account.gov.uk\"; flow:established; sid:2001; rev:1;)"
+    AllowedDomains       = "*.account.gov.uk"
+  }
+
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+}
