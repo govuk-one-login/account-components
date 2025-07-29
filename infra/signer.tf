38c7bc89ea853d692e8ad7ae45a92322dd22cf47
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-07-29 10:58:58 +0100

    OLH-2970: Deploy signer stack to build account

---
+++ infra/signer.tf
@@ -0,0 +1,14 @@
+resource "aws_cloudformation_stack" "signer_stack" {
+  count = var.create_build_stacks ? 1 : 0
+  # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3377004573/AWS+Signer
+  provider     = aws.london
+  name         = "signer"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/signer/template.yaml"
+
+  parameters = {
+    Environment = var.environment
+    System      = "Account Management Components"
+  }
+
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+}
