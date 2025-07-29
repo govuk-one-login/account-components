1af427b48eb663b82d702f71a60ad06b2dfb02b1
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-07-29 10:57:15 +0100

    OLH-2970: Deploy Github identity stack in build account

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,19 @@
+variable "environment" {
+  type        = string
+  description = "The environment name"
+  validation {
+    condition     = contains(["dev", "build", "staging", "integration", "production"], var.environment)
+    error_message = "Valid values for var: environment are (dev, build, staging, integration, production)."
+  }
+}
+
 variable "hosted_zone_domain" {
   type        = string
   description = "The base domain to use for the account's hosted zone"
+}
+
+variable "create_build_stacks" {
+  type        = bool
+  description = "Whether or not to deploy the stacks for building and signing application code. Only needed in dev and build. Defaults to false."
+  default     = false
 }
