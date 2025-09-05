e9c47260ade8012a38d64068727e7b02e6477e6f
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-09-05 11:26:45 +0100

    BAU: test path blocking

--- projects/app/src/api.ts
+++ projects/app/src/api.ts
@@ -21,10 +21,6 @@ export const api = function (app: FastifyInstance) {
           (await import("./handlers/api/hello/index.js")).hello(request, reply);
         },
       );
-
-      app.get("/", async function (request, reply) {
-        (await import("./handlers/api/hello/index.js")).hello(request, reply);
-      });
     },
     {
       prefix: "/api/",
