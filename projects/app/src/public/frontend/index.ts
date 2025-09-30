fb88265abdd32ad8ae69c39d26cba6c8ed88123d
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-09-30 08:51:14 +0100

    OLH-3230: configure session management

--- projects/app/src/public/frontend/index.ts
+++ projects/app/src/public/frontend/index.ts
@@ -5,19 +5,38 @@ import fastifyFormBody from "@fastify/formbody";
 import { getEnvironment } from "../../utils/getEnvironment/index.js";
 import type { FastifyTypeboxInstance } from "../../app.js";
 import { journeys } from "./journeys/index.js";
+import assert from "node:assert";
+import ConnectDynamoDB from "connect-dynamodb";
+import session from "express-session";
+import { ScalarAttributeType } from "@aws-sdk/client-dynamodb";
+import { getDynamoDbClient } from "../../utils/awsClient/index.js";
 
 export const frontend = function (app: FastifyTypeboxInstance) {
   app.register(fastifyFormBody);
   app.register(fastifyHelmet);
+
+  assert(process.env["SESSIONS_SECRET"]);
+  assert(process.env["SESSIONS_TABLE_NAME"]);
+
+  // @ts-expect-error - TODO
   app.register(fastifySession, {
-    secret: [
-      "TODO a secret with minimum length of 32 characters fron an env variable which is populated from a secret in secrets manager!!!!!",
-    ],
+    saveUninitialized: false,
+    secret: process.env["SESSIONS_SECRET"],
+    rolling: false,
+    store: new (ConnectDynamoDB(session))({
+      table: process.env["SESSIONS_TABLE_NAME"],
+      client: getDynamoDbClient().docClient,
+      specialKeys: [{ name: "user_id", type: ScalarAttributeType.S }],
+      skipThrowMissingSpecialKeys: true,
+      // TODO prefix needed?
+    }),
     cookie: {
       secure: getEnvironment() !== "local",
       sameSite: "lax",
+      maxAge: 1000000, // TODO
     },
   });
+
   app.register(fastifyCsrfProtection, {
     sessionPlugin: "@fastify/session",
   });
