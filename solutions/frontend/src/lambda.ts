6e3158b4dc873e16163b097f0defb88cdc4c2c45
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-10-15 09:07:50 +0100

    OLH-3293: update binary mime types

--- solutions/frontend/src/lambda.ts
+++ solutions/frontend/src/lambda.ts
@@ -4,7 +4,13 @@ import awsLambdaFastify from "@fastify/aws-lambda";
 const fastify = await initFrontend();
 
 export const handler = awsLambdaFastify(fastify, {
-  binaryMimeTypes: ["image/png", "font/woff", "font/woff2"],
+  binaryMimeTypes: [
+    "image/png",
+    "font/woff",
+    "font/woff2",
+    "image/x-icon",
+    "image/vnd.microsoft.icon",
+  ],
 });
 // needs to be placed after awsLambdaFastify call because of the decoration: https://github.com/fastify/aws-lambda-fastify/blob/main/index.js#L9
 await fastify.ready();
