36f6c6a62a5790fc1b09f0d2ffdf36b92626f991
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-10-17 16:58:10 +0100

    OLH-3147: initial work

---
+++ solutions/stubs/src/clientJwks/utils/buildJar/index.ts
@@ -0,0 +1,73 @@
+import { CompactEncrypt } from "jose";
+import { logger } from "../../../utils/logger.js";
+import { JWKS_KEY_TYPES, SignatureTypes } from "../../../types/common.js";
+import { convertPemToJwk } from "../../../utils/convert-pem-to-jwk.js";
+import { getKmsClient } from "../../../../../commons/utils/awsClient/index.js";
+import { createPublicKey } from "node:crypto";
+
+const getPublicKeyKmsAliasName = (keyType: SignatureTypes): string => {
+  const keyEnvironment =
+    keyType == SignatureTypes.EC
+      ? "JAR_EC_ENCRYPTION_KEY_ALIAS"
+      : "JAR_RSA_ENCRYPTION_KEY_ALIAS";
+
+  const publicKey = process.env[keyEnvironment];
+  if (!publicKey) {
+    throw new Error(`Environment variable ${keyEnvironment} is not set`);
+  }
+  return publicKey;
+};
+
+export async function buildJar(signedJwt: string): Promise<string> {
+  let publicKeyPem: string;
+  try {
+    const publicKey = await getKmsClient().getPublicKey({
+      KeyId: getPublicKeyKmsAliasName(SignatureTypes.RSA),
+    });
+
+    if (!publicKey.PublicKey) {
+      throw new Error("Public key data is missing from KMS response");
+    }
+
+    publicKeyPem = createPublicKey({
+      key: Buffer.from(publicKey.PublicKey),
+      format: "der",
+      type: "spki",
+    })
+      .export({
+        format: "pem",
+        type: "spki",
+      })
+      .toString();
+  } catch (error) {
+    logger.error(
+      `Failed to retrieve ${SignatureTypes.RSA} public key from SSM`,
+      { error },
+    );
+    throw new Error("Failed to retrieve key from SSM for param ");
+  }
+
+  if (!publicKeyPem) {
+    throw new Error("Public key PEM is empty");
+  }
+
+  const keyType = JWKS_KEY_TYPES.find((kt) => kt.kty === SignatureTypes.RSA);
+  if (!keyType) {
+    throw new Error(`Unsupported signature type: ${SignatureTypes.RSA}`);
+  }
+
+  const jwk = await convertPemToJwk(publicKeyPem, keyType);
+
+  const encryptedJwt = await new CompactEncrypt(
+    new TextEncoder().encode(signedJwt),
+  )
+    .setProtectedHeader({
+      alg: keyType.jweAlg,
+      enc: "A256GCM",
+      kid: keyType.kid,
+    })
+    .encrypt(jwk);
+
+  logger.info("Successfully build jar ");
+  return encryptedJwt;
+}
--- solutions/stubs/src/utils/paths.ts
+++ solutions/stubs/src/utils/paths.ts
@@ -1,4 +1,5 @@
 export const paths = {
   requestObjectGenerator: "/generate-request-object",
   requestObjectCreator: "/create-request-object",
+  clientJwks: "/:client/.well-known/jwks.json",
 } as const;
