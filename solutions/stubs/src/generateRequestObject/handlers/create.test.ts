619a14139bd9f3cbeb1003a054a53f417863fdbf
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2025-10-31 18:09:52 +0000

    BAU: add basic client callback stub

--- solutions/stubs/src/generateRequestObject/handlers/create.test.ts
+++ solutions/stubs/src/generateRequestObject/handlers/create.test.ts
@@ -97,7 +97,7 @@ describe("createRequestObjectPost", () => {
       "generateRequestObject/handlers/create.njk",
       expect.objectContaining({
         authorizeUrl:
-          "http://localhost:6004/authorize?client_id=23456789012345678901234567890123&scope=am-account-delete&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A6003%2Facm-callback&request=mock-request-object",
+          "http://localhost:6004/authorize?client_id=23456789012345678901234567890123&scope=am-account-delete&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A6003%2Fhome%2Fcallback&request=mock-request-object",
         jwtPayload: { foo: "bar" },
         jwtHeader: { alg: "ES256" },
         originalRequest: mockRequest.body,
