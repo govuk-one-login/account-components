3c52ab00c0069ef41d580f23aa6805e35506bd6c
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2025-11-20 19:05:46 +0000

    OLH-3396:journey outcome claims and signature verification
    Extract access token from Authorization header and verify that it has
    the expected claims with valid values as outlined in the doc.
    Use JWTSigningKey to verify the signature of the token.
    
    Log warnings and return HTTP 400 errors where the token is deemed to
    be invalid or otherwise malformed.

---
+++ solutions/api/src/lambda/journeyoutcome/utils/verifySignatureAndGetPayload.ts
@@ -0,0 +1,47 @@
+import { errorManager } from "./errors.js";
+import type { CryptoKey, JWTVerifyResult, JWTHeaderParameters } from "jose";
+import { JWSSignatureVerificationFailed, JWTInvalid } from "jose/errors";
+import { jwtVerify, decodeJwt } from "jose";
+import { jwtSigningAlgorithm } from "../../../../../commons/utils/constants.js";
+import type { JourneyInfoPayload } from "./validateJourneyOutcomeJwtClaims.js";
+export async function verifySignatureAndGetPayload(
+  token: string,
+  key: CryptoKey,
+): Promise<JourneyInfoPayload> {
+  try {
+    const { payload }: JWTVerifyResult<JourneyInfoPayload> = await jwtVerify(
+      token,
+      key,
+      {
+        algorithms: [jwtSigningAlgorithm],
+      },
+    );
+    return payload;
+  } catch (error) {
+    if (error instanceof JWTInvalid) {
+      errorManager.throwError(
+        "InvalidAccessToken",
+        `Access token is malformed or invalid`,
+      );
+    } else if (error instanceof JWSSignatureVerificationFailed) {
+      const payload: JourneyInfoPayload = decodeJwt(token);
+      const parts = token.split(".");
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const header: JWTHeaderParameters = JSON.parse(
+        Buffer.from(parts[0] ?? "", "base64").toString("utf-8"),
+      );
+      const jti: string = payload.jti ?? "Not found";
+      const kid: string = header.kid ?? "Not found";
+      errorManager.throwError(
+        "AccessTokenSignatureInvalid",
+        `Invalid access token signature with jti: ${jti} and kid: ${kid}`,
+      );
+    } else {
+      throw error;
+    }
+  }
+  // Line below is unreachable. It's to appease TS
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  return {} as JourneyInfoPayload;
+}
