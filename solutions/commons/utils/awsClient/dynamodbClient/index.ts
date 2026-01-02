ceee8d7ccf6ccca3f8f5f001856fad02050922c8
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2026-01-02 14:56:22 +0000

    BAU: update log group name

--- solutions/commons/utils/awsClient/dynamodbClient/index.ts
+++ solutions/commons/utils/awsClient/dynamodbClient/index.ts
@@ -21,58 +21,58 @@ const createDynamoDbClient = () => {
   logger.info("client config", config)
   const dynamoDbClient = new DynamoDBClient(config);
   logger.info("client created", { dynamoDbClient });
+  
+  const docClient = DynamoDBDocumentClient.from(dynamoDbClient);
+  logger.info("docClient created", { docClient });
 
-  let wrappedClient: DynamoDBClient;
+  let wrappedClient: DynamoDBDocumentClient;
   logger.info("env", getEnvironment())
   if (getEnvironment() === "local") {
-    wrappedClient = dynamoDbClient;
+    wrappedClient = docClient;
     logger.info("Without X Ray")
   } else {
-    wrappedClient = AWSXRay.captureAWSv3Client(dynamoDbClient);
+    wrappedClient = AWSXRay.captureAWSv3Client(docClient);
     logger.info("With X Ray")
   }
 
-  const docClient = DynamoDBDocumentClient.from(wrappedClient);
-  logger.info("docClient created", { docClient });
-
   const client = {
-    client: docClient,
-    config: docClient.config,
+    client: wrappedClient,
+    config: wrappedClient.config,
     put: async (params: PutCommandInput) => {
       const { PutCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new PutCommand(params));
+      return await wrappedClient.send(new PutCommand(params));
     },
     get: async (params: GetCommandInput) => {
       const { GetCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new GetCommand(params));
+      return await wrappedClient.send(new GetCommand(params));
     },
     delete: async (params: DeleteCommandInput) => {
       const { DeleteCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new DeleteCommand(params));
+      return await wrappedClient.send(new DeleteCommand(params));
     },
     update: async (params: UpdateCommandInput) => {
       const { UpdateCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new UpdateCommand(params));
+      return await wrappedClient.send(new UpdateCommand(params));
     },
     query: async (params: QueryCommandInput) => {
       const { QueryCommand } = await import("@aws-sdk/client-dynamodb");
-      return await docClient.send(new QueryCommand(params));
+      return await wrappedClient.send(new QueryCommand(params));
     },
     scan: async (params: ScanCommandInput) => {
       const { ScanCommand } = await import("@aws-sdk/client-dynamodb");
-      return await docClient.send(new ScanCommand(params));
+      return await wrappedClient.send(new ScanCommand(params));
     },
     batchWrite: async (params: BatchWriteCommandInput) => {
       const { BatchWriteCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new BatchWriteCommand(params));
+      return await wrappedClient.send(new BatchWriteCommand(params));
     },
     batchGet: async (params: BatchGetCommandInput) => {
       const { BatchGetCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new BatchGetCommand(params));
+      return await wrappedClient.send(new BatchGetCommand(params));
     },
     transactWrite: async (params: TransactWriteCommandInput) => {
       const { TransactWriteCommand } = await import("@aws-sdk/lib-dynamodb");
-      return await docClient.send(new TransactWriteCommand(params));
+      return await wrappedClient.send(new TransactWriteCommand(params));
     },
   };
   return client;
