1cc4e66942da4c8611515a5fc3b9f4c659b7233a
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2026-01-02 15:57:01 +0000

    BAU: no joy

--- solutions/spike/handler.ts
+++ solutions/spike/handler.ts
@@ -1,20 +1,16 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 // eslint-disable-next-line no-restricted-imports
 import { Logger } from "@aws-lambda-powertools/logger";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { randomUUID } from "node:crypto";
-import * as AWSXRay from "aws-xray-sdk";
 import { getDynamoDbClient } from "../commons/utils/awsClient/dynamodbClient/index.js";
-import {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} from "@aws-sdk/lib-dynamodb";
-import { getAwsClientConfig } from "../commons/utils/awsClient/getAwsClientConfig/index.js";
+import { loggerAPIGatewayProxyHandlerWrapper } from "../commons/utils/logger/index.js";
+import { metricsAPIGatewayProxyHandlerWrapper } from "../commons/utils/metrics/index.js";
 
 const logger = new Logger();
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = loggerAPIGatewayProxyHandlerWrapper(
+  metricsAPIGatewayProxyHandlerWrapper(
+    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     logger.info("event", { event });
     // const dbClient = AWSXRay.captureAWSv3Client(new DynamoDBClient(getAwsClientConfig()))
     // const db = DynamoDBDocumentClient.from(dbClient);
@@ -22,13 +18,18 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     logger.info("db.config", { config: db.config })
     const nonce = randomUUID()
     const expiry = Math.floor(Date.now() / 1000) + 60
-    await db.put({
-      TableName: process.env["REPLAY_ATTACK_TABLE_NAME"],
-      Item: {
-        nonce: nonce,
-        expires: expiry
-      },
-      ConditionExpression: "attribute_not_exists(nonce)",
+    await db.transactWrite({
+      TransactItems: [
+        {
+          Put: {
+            TableName: process.env["REPLAY_ATTACK_TABLE_NAME"],
+            Item: {
+              nonce: nonce,
+              expires: expiry
+            },
+            ConditionExpression: "attribute_not_exists(nonce)",
+           }
+        }]
     })
     const result = await db.get({
       TableName: process.env["REPLAY_ATTACK_TABLE_NAME"],
@@ -40,4 +41,4 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       statusCode: 200,
       body: JSON.stringify(result)
     }
-};
+}));
