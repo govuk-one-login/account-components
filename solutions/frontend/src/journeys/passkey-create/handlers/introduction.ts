7431950990ebdea1c3593a54a65f0902f6d5b578
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2026-01-08 16:13:18 +0000

    BAU: passkey wip

--- solutions/frontend/src/journeys/passkey-create/handlers/introduction.ts
+++ solutions/frontend/src/journeys/passkey-create/handlers/introduction.ts
@@ -3,10 +3,10 @@ import assert from "node:assert";
 
 const render = async (reply: FastifyReply, options?: object) => {
   assert.ok(reply.render);
-  await reply.render(
-    "journeys/passkey-create/templates/introduction.njk",
-    options,
-  );
+  await reply.render("journeys/passkey-create/templates/introduction.njk", {
+    backLink: "TODO",
+    ...options,
+  });
 };
 
 export async function introductionGetHandler(
