de85205e35966151ace3a193539def68badbeb6a
Jeremy Barrass <jeremy.barrass@digital.cabinet-office.gov.uk>
2026-01-15 13:09:18 +0000

    OLH-3558 Add BASH script to get user IDs from Activity Log, add Lambda to get Activity History

---
+++ solutions/api/src/lambda/deleteinactive/index.test.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect, vi } from "vitest";
+import { handler } from "./index.js";
+import type { APIGatewayProxyEvent, Context } from "aws-lambda";
+
+// @ts-expect-error
+vi.mock(import("../../../../commons/utils/logger/index.js"), () => ({
+  logger: {},
+  loggerAPIGatewayProxyHandlerWrapper: (fn) => fn,
+}));
+
+// @ts-expect-error
+vi.mock(import("../../../../commons/utils/metrics/index.js"), () => ({
+  metrics: {},
+  metricsAPIGatewayProxyHandlerWrapper: (fn) => fn,
+}));
+
+vi.mock(import("@aws-sdk/client-dynamodb"), () => ({
+  DynamoDBClient: vi.fn().mockImplementation(() => ({})),
+}));
+
+// eslint-disable-next-line vitest/prefer-import-in-mock
+vi.mock("@aws-sdk/lib-dynamodb", async (importOriginal) => {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+  const actual = await importOriginal<typeof import("@aws-sdk/lib-dynamodb")>();
+
+  return {
+    ...actual,
+    DynamoDBDocumentClient: {
+      // eslint-disable-next-line @typescript-eslint/no-misused-spread
+      ...actual.DynamoDBDocumentClient,
+      from: vi.fn().mockImplementation(() => ({
+        send: vi.fn(),
+      })),
+    },
+    paginateScan: vi.fn().mockImplementation(async function* () {
+      yield { Items: [] };
+    }),
+  };
+});
+
+describe("getInactiveUsers handler", () => {
+  it("returns 200 status with items body", async () => {
+    const result = await handler({} as APIGatewayProxyEvent, {} as Context);
+
+    expect(result).toStrictEqual({
+      statusCode: 200,
+      body: JSON.stringify([]),
+    });
+  });
+});
---
+++ solutions/api/src/lambda/deleteinactive/index.ts
@@ -0,0 +1,73 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { metricsAPIGatewayProxyHandlerWrapper } from "../../../../commons/utils/metrics/index.js";
+import {
+  logger,
+  loggerAPIGatewayProxyHandlerWrapper,
+} from "../../../../commons/utils/logger/index.js";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, paginateScan } from "@aws-sdk/lib-dynamodb";
+import { ErrorManager } from "../../utils/common.js";
+import type { ErrorType } from "../../utils/common.js";
+
+export const handler = loggerAPIGatewayProxyHandlerWrapper(
+  metricsAPIGatewayProxyHandlerWrapper(
+    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+      const dynamodbClient = new DynamoDBClient({});
+      const activityLogClient = DynamoDBDocumentClient.from(dynamodbClient);
+      const errorManager = new ErrorManager<Record<"ScanError", ErrorType>>({
+        ScanError: {
+          code: "ScanError",
+          description: "Failed to scan activity log",
+          statusCode: 500,
+        },
+      });
+      logger.info(
+        `Starting to fetch inactive users from activity log: ${event.body ?? "no body"}`,
+      );
+
+      const getActivityHistory = async () => {
+        const params = {
+          TableName: "activity_log",
+          FilterExpression: "#year <= :fiveYearsAgo",
+          ExpressionAttributeNames: {
+            "#year": "timestamp",
+          },
+          ExpressionAttributeValues: {
+            ":fiveYearsAgo": new Date().setFullYear(
+              new Date().getFullYear() - 5,
+            ),
+          },
+        };
+
+        const paginatedScan = paginateScan(
+          { client: activityLogClient },
+          params,
+        );
+        const lastActiveFiveYearsAgo = [];
+
+        try {
+          for await (const page of paginatedScan) {
+            if (page.Items) {
+              lastActiveFiveYearsAgo.push(...page.Items);
+            }
+          }
+        } catch (error) {
+          logger.error("Error scanning activity log table", { error });
+          errorManager.throwError(
+            "ScanError",
+            "Error scanning activity log table",
+          );
+        }
+        return lastActiveFiveYearsAgo;
+      };
+      const inactiveUsers = await getActivityHistory();
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          inactiveUsers,
+        }),
+      };
+    },
+  ),
+);
