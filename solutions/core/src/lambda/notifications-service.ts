6747bc73e6ad334634f9ceb27fd82d13005f4b4e
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-02-03 17:08:52 +0000

    OLH-3661: improvements

--- solutions/core/src/lambda/notifications-service.ts
+++ solutions/core/src/lambda/notifications-service.ts
@@ -45,20 +45,30 @@ let notifyClient: NotifyClientType | undefined = undefined;
 const setUpNotifyClient = async (
   record: SQSRecord,
 ): Promise<NotifyClientType | undefined> => {
+  console.log("MHTEST13");
+
   if (notifyClient) {
     return notifyClient;
   }
+
+  console.log("MHTEST14");
 
   assert.ok(
     process.env["NOTIFY_API_KEY_SECRET_ARN"],
     "process.env.NOTIFY_API_KEY_SECRET_ARN is not defined",
   );
+  console.log("MHTEST15");
+
   const notifyApiKeySecretArn = process.env["NOTIFY_API_KEY_SECRET_ARN"];
   const appConfig = await getAppConfig();
+
+  console.log("MHTEST16");
 
   const notifyApiKey = await getSecret(notifyApiKeySecretArn, {
     maxAge: appConfig.notify_api_key_scret_max_age,
   });
+
+  console.log("MHTEST17");
 
   if (!notifyApiKey) {
     const errorName = "notify_api_key_is_undefined";
@@ -69,6 +79,8 @@ const setUpNotifyClient = async (
     addSendNotificationFailedMetric(errorName);
     return;
   }
+  console.log("MHTEST18");
+
   if (
     typeof notifyApiKey !== "string" // pragma: allowlist secret
   ) {
@@ -80,9 +92,12 @@ const setUpNotifyClient = async (
     addSendNotificationFailedMetric(errorName);
     return;
   }
+  console.log("MHTEST19");
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
   notifyClient = new NotifyClient(notifyApiKey);
+
+  console.log("MHTEST20");
 
   return notifyClient;
 };
@@ -99,14 +114,20 @@ let notifyTemplateIds:
 const getNotifyTemplateIds = (
   record: SQSRecord,
 ): v.InferOutput<typeof notifyTemplateIDsSchema> | undefined => {
+  console.log("MHTEST3");
+
   if (notifyTemplateIds) {
     return notifyTemplateIds;
   }
+
+  console.log("MHTEST4");
 
   const templateIds = v.safeParse(
     notifyTemplateIDsSchema,
     process.env["NOTIFY_TEMPLATE_IDS"],
   );
+
+  console.log("MHTEST5");
 
   if (!templateIds.success) {
     const errorName = "invalid_template_ids_format";
@@ -118,6 +139,8 @@ const getNotifyTemplateIds = (
     return;
   }
 
+  console.log("MHTEST6");
+
   notifyTemplateIds = templateIds.output;
 
   return notifyTemplateIds;
@@ -127,18 +150,26 @@ const processNotification = async (
   record: SQSRecord,
   batchItemFailures: SQSBatchItemFailure[],
 ) => {
+  console.log("MHTEST2");
+
   try {
     const notifyTemplateIds = getNotifyTemplateIds(record);
+
+    console.log("MHTEST7");
 
     if (!notifyTemplateIds) {
       batchItemFailures.push({ itemIdentifier: record.messageId });
       return;
     }
 
+    console.log("MHTEST8");
+
     const messageParsed = v.safeParse(
       v.pipe(v.string(), v.parseJson(), messageSchema),
       record.body,
     );
+    console.log("MHTEST9");
+
     if (!messageParsed.success) {
       const errorName = "invalid_message_format";
       logger.error(errorName, {
@@ -148,6 +179,7 @@ const processNotification = async (
       batchItemFailures.push({ itemIdentifier: record.messageId });
       return;
     }
+    console.log("MHTEST10");
 
     const message: {
       emailAddress: EmailAddress;
@@ -156,6 +188,9 @@ const processNotification = async (
     } = messageParsed.output;
 
     const templateId = notifyTemplateIds[message.notificationType];
+
+    console.log("MHTEST11");
+
     if (!templateId) {
       const errorName = "template_id_not_found";
       logger.error(errorName, {
@@ -167,12 +202,18 @@ const processNotification = async (
       return;
     }
 
+    console.log("MHTEST12");
+
     const notifyClient = await setUpNotifyClient(record);
+
+    console.log("MHTEST21");
 
     if (!notifyClient) {
       batchItemFailures.push({ itemIdentifier: record.messageId });
       return;
     }
+
+    console.log("MHTEST22");
 
     let sendResult: unknown;
     try {
@@ -184,8 +225,13 @@ const processNotification = async (
           reference: randomUUID(),
         },
       );
+      console.log("MHTEST23");
     } catch (error) {
+      console.log("MHTEST24");
+
       if (isAxiosError(error)) {
+        console.log("MHTEST25");
+
         const errorName = "unable_to_send_notification";
         logger.error(errorName, {
           messageId: record.messageId,
@@ -196,6 +242,8 @@ const processNotification = async (
         });
         addSendNotificationFailedMetric(errorName);
       } else {
+        console.log("MHTEST26");
+
         const errorName = "unable_to_send_notification_due_to_an_unknown_error";
         logger.error(errorName, {
           messageId: record.messageId,
@@ -204,9 +252,12 @@ const processNotification = async (
         });
         addSendNotificationFailedMetric(errorName);
       }
+      console.log("MHTEST27");
+
       batchItemFailures.push({ itemIdentifier: record.messageId });
       return;
     }
+    console.log("MHTEST28");
 
     const notifySuccessSchema = v.object({
       data: v.object({
@@ -214,6 +265,8 @@ const processNotification = async (
         reference: v.nullish(v.string()),
       }),
     });
+
+    console.log("MHTEST29");
 
     const resultParsed = v.safeParse(notifySuccessSchema, sendResult);
     if (!resultParsed.success) {
@@ -227,6 +280,8 @@ const processNotification = async (
       return;
     }
 
+    console.log("MHTEST30");
+
     logger.info("notification_sent", {
       messageId: record.messageId,
       id: resultParsed.output.data.id,
@@ -234,7 +289,11 @@ const processNotification = async (
       notificationType: message.notificationType,
     });
     metrics.addMetric("SendNotificationSucceeded", MetricUnit.Count, 1);
+
+    console.log("MHTEST30");
   } catch (error) {
+    console.log("MHTEST31");
+
     const errorName = "unknown_error";
     logger.error(errorName, {
       messageId: record.messageId,
@@ -244,10 +303,13 @@ const processNotification = async (
     batchItemFailures.push({ itemIdentifier: record.messageId });
     return;
   }
+  console.log("MHTEST32");
 };
 
 export const handler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
   const batchItemFailures: SQSBatchItemFailure[] = [];
+
+  console.log("MHTEST1");
 
   await Promise.allSettled(
     event.Records.map((record) =>
@@ -255,9 +317,13 @@ export const handler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
     ),
   );
 
+  console.log("MHTEST33");
+
   logger.resetKeys();
   metrics.captureColdStartMetric();
   metrics.publishStoredMetrics();
+
+  console.log("MHTEST34");
 
   return {
     batchItemFailures,
