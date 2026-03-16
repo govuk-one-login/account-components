a99be7ec3a92605722c5d19a4ceedb46e2cfe906
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2026-03-16 21:10:30 +0000

    OLH-3945: Fixed integration tests to work with new API url

--- solutions/integration-tests/utils/getApiBaseUrl.ts
+++ solutions/integration-tests/utils/getApiBaseUrl.ts
@@ -1,13 +1,21 @@
 import { env } from "../env.js";
+import { getParameter } from "@aws-lambda-powertools/parameters/ssm";
 
-export const getApiBaseUrl = () => {
+export const getApiBaseUrl = async () => {
   if (env.TEST_TARGET === "local") {
     return "http://localhost:6004";
   }
 
-  if (env.TEST_TARGET === "production") {
-    return "https://api.manage.account.gov.uk";
+  try {
+    const apiBaseUrl = await getParameter(
+      "/tests/components-api/PrivateApiGatewayUrl",
+      { maxAge: 300 },
+    );
+    if (!apiBaseUrl || apiBaseUrl.trim().length <= 0) {
+      throw new Error("API base URL from SSM is empty");
+    }
+    return apiBaseUrl;
+  } catch (error) {
+    throw new Error(`Failed to get API base URL from SSM`, { cause: error });
   }
-
-  return `https://api.manage.${env.TEST_TARGET}.account.gov.uk`;
 };
