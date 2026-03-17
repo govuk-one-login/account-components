4cb2582b789d4761ff8b12a8ede8dc0419bc37a1
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-03-17 14:02:43 +0000

    BAU: tidying up

--- solutions/infra/pipelines.tf
+++ solutions/infra/pipelines.tf
@@ -102,31 +102,6 @@ resource "aws_cloudformation_stack" "core_pipeline_stack" {
   depends_on   = [aws_cloudformation_stack.vpc_stack, aws_cloudformation_stack.build_notifications_stack]
 }
 
-resource "aws_cloudformation_stack" "alarms_pipeline_stack" {
-  name         = "pipeline-alarms"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/sam-deploy-pipeline/template.yaml"
-
-  parameters = {
-    SAMStackName                            = "components-alarms"
-    Environment                             = var.environment
-    VpcStackName                            = "vpc"
-    SigningProfileArn                       = var.signing_profile_arn
-    SigningProfileVersionArn                = var.signing_profile_version_arn
-    AdditionalCodeSigningVersionArns        = var.additional_code_signing_version_arns
-    CustomKmsKeyArns                        = var.custom_kms_key_arn
-    ArtifactSourceBucketArn                 = var.alarms_artifact_source_bucket_arn
-    ArtifactSourceBucketEventTriggerRoleArn = var.alarms_artifact_source_bucket_event_trigger_role_arn
-    GitHubRepositoryName                    = var.create_build_stacks ? var.repository_name : "none"
-    IncludePromotion                        = contains(["build", "staging"], var.environment) ? "Yes" : "No"
-    AllowedAccounts                         = join(",", var.allowed_promotion_accounts)
-    BuildNotificationStackName              = "build-notifications"
-    SlackNotificationType                   = var.environment == "production" ? "All" : (var.environment == "dev" ? "None" : "Failures")
-  }
-
-  capabilities = var.capabilities
-  depends_on   = [aws_cloudformation_stack.vpc_stack, aws_cloudformation_stack.build_notifications_stack]
-}
-
 resource "aws_cloudformation_stack" "mocks_pipeline_stack" {
   count        = contains(["build", "dev"], var.environment) ? 1 : 0
   name         = "pipeline-mocks"
