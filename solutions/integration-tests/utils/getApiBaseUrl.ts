7e6270172a7619226ae4260fd2744385f9e1d80e
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-04-22 10:00:07 +0100

    BAU: attempt integration test fix

--- solutions/integration-tests/utils/getApiBaseUrl.ts
+++ solutions/integration-tests/utils/getApiBaseUrl.ts
@@ -7,7 +7,7 @@ export const getApiBaseUrl = async () => {
   }
 
   try {
-    const apiBaseUrl = await getParameter("/amc/PrivateApiGatewayUrl", {
+    const apiBaseUrl = await getParameter("/amc/tests/PrivateApiGatewayUrl", {
       maxAge: 300,
     });
     if (!apiBaseUrl || apiBaseUrl.trim().length <= 0) {
