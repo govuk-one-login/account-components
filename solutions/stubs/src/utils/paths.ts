538d3da734b1ce1e54f5af33d7497d64977040e5
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-04-27 09:52:35 +0100

    OLH-4059: fix stub path

--- solutions/stubs/src/utils/paths.ts
+++ solutions/stubs/src/utils/paths.ts
@@ -18,6 +18,6 @@ export const paths = {
       "/account-data-api/accounts/:publicSubjectId/authenticators/passkeys",
   },
   notify: {
-    sendEmail: "/notify/*",
+    sendEmail: "/v2/notifications/email",
   },
 } as const;
