dbc21be57a1ba4ed18c251577b27495cb7df513e
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-06-11 14:34:27 +0100

    BAU: update event catalogue dependencies

--- solutions/frontend/src/journeys/passkey-create/handlers/create.test.ts
+++ solutions/frontend/src/journeys/passkey-create/handlers/create.test.ts
@@ -502,7 +502,7 @@ describe("passkey-create handlers", () => {
         );
         expect(
           mockSendPasskeyRegistrationFailedAuditEvent,
-        ).toHaveBeenCalledWith(mockRequest, mockReply, "InvalidRequestBody");
+        ).toHaveBeenCalledWith(mockRequest, mockReply);
         expect(
           mockSendPasskeyRegistrationFailedAuditEvent,
         ).toHaveBeenCalledTimes(1);
