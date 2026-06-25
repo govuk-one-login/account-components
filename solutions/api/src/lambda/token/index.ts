651eb309ebeb206209d9241fd19878f584de4fbc
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-06-25 22:22:49 +0100

    BAU: debugging

--- solutions/api/src/lambda/token/index.ts
+++ solutions/api/src/lambda/token/index.ts
@@ -63,6 +63,7 @@ export const handler = normalizeAPIGatewayProxyEventHandlerWrapper(
             }),
           };
         } catch (error) {
+          console.error("MHTEST", error);
           // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
           return errorManager.handleError(error as TokenAppError | Error);
         }
