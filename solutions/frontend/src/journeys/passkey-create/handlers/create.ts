9de10b6e6c9ec1ba7b9692e5e5056354a53f7c19
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-07-02 15:48:35 +0100

    BAU: temp logging change

--- solutions/frontend/src/journeys/passkey-create/handlers/create.ts
+++ solutions/frontend/src/journeys/passkey-create/handlers/create.ts
@@ -223,6 +223,7 @@ export async function postHandler(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
+  request.log.warn("MHTEST - try to get tests working with new image");
   assert.ok(reply.journeyStates?.["passkey-create"]);
 
   const registrationOptions =
