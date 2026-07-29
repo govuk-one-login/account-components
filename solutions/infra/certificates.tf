ae9ae18f7153b222fc231a93ee14622faeac09f1
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-07-29 09:59:22 +0100

    BAU: pin dev platform stacks to major version

--- solutions/infra/certificates.tf
+++ solutions/infra/certificates.tf
@@ -2,7 +2,7 @@ resource "aws_cloudformation_stack" "certificate_stack_virginia" {
   # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/4028006740/certificate+readme
   provider     = aws.virginia
   name         = "certificate"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/certificate/template.yaml"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/certificate/template-v1.yaml"
 
   parameters = {
     DomainName   = var.hosted_zone_domain
@@ -13,7 +13,7 @@ resource "aws_cloudformation_stack" "certificate_stack_virginia" {
 resource "aws_cloudformation_stack" "certificate_stack_london" {
   # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/4028006740/certificate+readme
   name         = "certificate"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/certificate/template.yaml"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/certificate/template-v1.yaml"
 
   parameters = {
     DomainName           = var.hosted_zone_domain
--- solutions/infra/signer.tf
+++ solutions/infra/signer.tf
@@ -2,7 +2,7 @@ resource "aws_cloudformation_stack" "signer_stack" {
   count = var.create_build_stacks ? 1 : 0
   # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3377004573/AWS+Signer
   name         = "signer"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/signer/template.yaml"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/signer/template-v1.yaml"
 
   parameters = {
     Environment = var.environment
@@ -16,7 +16,7 @@ resource "aws_cloudformation_stack" "container_signer_stack" {
   count = var.create_build_stacks ? 1 : 0
   # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3376840727/Container+Signer
   name         = "container-signer"
-  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/container-signer/template.yaml"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/container-signer/template-v1.yaml"
 
   parameters = {
     Environment     = var.environment
